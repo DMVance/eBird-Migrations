bae56a5e985d7e316f8fa8bcf5cc84fd8cc5b38b
DMVance <csljrrt@gmail.com>
2021-08-01 10:17:13 -0500

    create python file for app

--- migrations.py
+++ migrations.py
@@ -1,1 +1,87 @@
 # Data analysis and backend engine
+
+import os, csv, requests, json, datetime
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from config import api_key, mapbox_token
+from datetime import date
+
+px.set_mapbox_access_token(mapbox_token)
+
+today = date.today()
+days_back_short = 3
+days_back_long = 30
+
+# ALL notable observations in Texas
+regionCode = "US-TX"
+
+url_notables = (
+    "https://api.ebird.org/v2/data/obs/"
+    + regionCode
+    + f"/recent/notable?back={days_back_long}"  # max number of days back for observations = 30
+)
+
+r_notables = requests.get(
+    url_notables, params={"key": api_key, "back": days_back_long, "detail": "full"}
+)
+
+df_notables = pd.DataFrame(r_notables.json())
+
+pd.to_datetime(df_notables["obsDt"])
+df_notables["Year"] = pd.DatetimeIndex(df_notables["obsDt"]).year
+df_notables["Month"] = pd.DatetimeIndex(df_notables["obsDt"]).month
+df_notables["Day"] = pd.DatetimeIndex(df_notables["obsDt"]).day
+
+columns_dict = {
+    "speciesCode": "Species_Code",
+    "comName": "Common_Name",
+    "sciName": "Scientific_Name",
+    "obsDt": "Date",
+    "howMany": "Bird_Count",
+    "subnational2Code": "City_Code",
+    "subnational2Name": "City_Name",
+    "subnational1Code": "State_Code",
+    "subnational1Name": "State_Name",
+    "countryCode": "Country_Code",
+    "countryName": "Country_Name",
+}
+
+df_notables = df_notables.rename(columns=columns_dict)
+
+df_notables["Bird_Count"].fillna(value=1, inplace=True)
+
+df_notables.to_csv("Resources/TX_NotableObs_eBird.csv", index=False)
+
+TX_NOTABLE_OBS = os.path.join("Resources", "TX_NotableObs_eBird.csv")
+TX_NOTABLE_OBS_df = pd.read_csv(TX_NOTABLE_OBS)
+
+TX_NOTABLE_OBS_df.columns
+
+TX_NOTABLE_OBS_df.drop_duplicates(inplace=True)
+
+# mapbox_style options:
+#     'open-street-map'`,
+#     `'white-bg'`, `'carto-positron'`, `'carto-darkmatter'`, `'stamen-
+#     terrain'`, `'stamen-toner'`, `'stamen-watercolor'`. 
+
+# Allowed values which do require a Mapbox API token are `'basic'`, `'streets'`,
+#     `'outdoors'`, `'light'`, `'dark'`, `'satellite'`, `'satellite-
+#     streets'`
+
+# Mapping
+fig_TX_NOTABLE_OBS_df = px.scatter_mapbox(
+    TX_NOTABLE_OBS_df,
+    lat="lat",
+    lon="lng",
+    color="Common_Name",
+    #     size="Bird_Count",
+    hover_name="Common_Name",
+    hover_data=["Bird_Count", "Date", "obsValid"],
+    title=f"All Notable Observations in Texas: last {days_back_long} days (Run date: {today})",
+    mapbox_style="stamen-terrain",
+    width=1000,
+    height=650,
+)
+fig_TX_NOTABLE_OBS_df.show()
+
